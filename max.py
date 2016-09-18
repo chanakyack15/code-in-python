167796cab51f3d9d117ad439518adcbb9e8ea160
Chanakya khatri <ck150895@gmail.com>
2016-09-18 12:01:32 +0530

    to find the max and min(type casting)

---
+++ max.py
@@ -0,0 +1,13 @@
+
+value=int(input("enter 1st no"))
+value1=int(input("enter 2nd no"))
+value2=int(input("enter 3rd no"))
+
+print ("MAX no in all 3 no is:")
+print (max(value,value1,value2))
+print ("MIN no in all 3 no is:")
+print (min(value,value1,value2))
+
+input("enter to exit")
+
+
